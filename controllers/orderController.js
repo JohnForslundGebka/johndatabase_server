8bd403ac0a843896612e386b1ec498b6f852e1f8
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-11-13 11:28:02 +0100

    start work with orderController

---
+++ controllers/orderController.js
@@ -0,0 +1,14 @@
+const db = require('../config/db');
+
+exports.createOrder = (req, res) => {
+
+}
+
+exports.getOrderById = (req, res) => {
+
+}
+
+
+exports.getAllOrders = (req, res) => {
+    
+}
